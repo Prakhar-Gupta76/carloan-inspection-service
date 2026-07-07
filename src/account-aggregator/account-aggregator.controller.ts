900f4a6389bdd3c09bf4ce389e1f56b0770c706f
Prakhar-Gupta <pg0002003@gmail.com>
2026-07-07 17:38:54 +0530

    API Endpoint URL Changed

--- src/account-aggregator/account-aggregator.controller.ts
+++ src/account-aggregator/account-aggregator.controller.ts
@@ -2,11 +2,11 @@ import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AccountAggregatorService } from './account-aggregator.service';
 import { AccountAggregatorRequestDto } from './dto/account-aggregator-request.dto';
 
-@Controller('api/v1/account-aggregator')
+@Controller('api/v1/salary-verification')
 export class AccountAggregatorController {
   constructor(
     private readonly accountAggregatorService: AccountAggregatorService,
-  ) {}
+  ) { }
 
   @Post()
   getBankStatement(
